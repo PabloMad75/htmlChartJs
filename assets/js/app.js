90c3c01a9a15ca9e5d5f672c6771e3d50aaceb4c
Mauricio Espinoza P <espinoza.p.mauricio@gmail.com>
2023-06-17 10:37:15 -0400

    Add chartjs

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+const ctx = document.getElementById("myChart");
+  
+new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange","Pink"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, -53, 5, 2000, 3, 25],
+        borderWidth: 4,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
