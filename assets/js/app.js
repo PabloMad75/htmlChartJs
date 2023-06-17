c5584a015f299a10e33c0374909f4e51f82c62dd
Mauricio Espinoza P <espinoza.p.mauricio@gmail.com>
2023-06-17 11:13:34 -0400

    Add background color, refactorize

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,56 @@
-const ctx = document.getElementById("myChart");
+document.addEventListener("DOMContentLoaded", () => {
+  const data = {
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
+    values: [10, 20, 15, 25, 30, 18],
+  };
 
-new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange","Pink"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, -53, 5, 2000, 3, 25],
-        borderWidth: 4,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+  const ctx = document.getElementById("myChart");
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: data.labels,
+      datasets: [
+        {
+          label: "# of Month",
+          data: data.values,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.5)",
+
+            "rgba(54, 162, 235, 0.5)",
+
+            "rgba(255, 206, 86, 0.5)",
+
+            "rgba(75, 192, 192, 0.5)",
+
+            "rgba(153, 102, 255, 0.5)",
+
+            "rgba(255, 159, 64, 0.5)",
+          ],
+
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+
+            "rgba(54, 162, 235, 1)",
+
+            "rgba(255, 206, 86, 1)",
+
+            "rgba(75, 192, 192, 1)",
+
+            "rgba(153, 102, 255, 1)",
+
+            "rgba(255, 159, 64, 1)",
+          ],
+
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
     },
-  },
+  });
 });
